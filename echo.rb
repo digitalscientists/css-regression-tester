f97afceb6b8de0cd9f0edd2187ee8a82788717fd
Fadi Eliya <fadi.eliya@digitalscientists.com>
2015-03-02 15:53:31 -0500

    [#88709808] Adds test for Survey Editor
    - adds test for My Surveys on Dashboard

--- echo.rb
+++ echo.rb
@@ -20,6 +20,7 @@ def driver_for(env)
     url: SAUCE_URL, 
     desired_capabilities: caps
   )
+  # Selenium::WebDriver.for(:firefox)
 end
 
 describe "Echo Test Suite" do
@@ -43,7 +44,6 @@ describe "Echo Test Suite" do
   end
 
   after(:all) do
-    @driver.quit
   end
   
   environments.each do |env|
@@ -60,9 +60,17 @@ describe "Echo Test Suite" do
       ) do |driver|
 
         driver.get(@base_url + "/")
+
         @eyes.check_window('Dashboard')
+
+        driver.execute_script(%|
+          document.querySelector('.page-container').scrollTop = 600
+        |)
+        @eyes.check_window('My Surveys')
+
         driver.find_element(:link, "New Survey").click
         @eyes.check_window('New Survey Menu')
+
         driver.find_element(:css, "a.left-off-canvas-toggle.menu-icon").click
         @eyes.check_window('Main Menu')
 
@@ -72,7 +80,8 @@ describe "Echo Test Suite" do
 
     end
 
-    it "tests the select schools on #{env}" do
+    it "tests the Select Schools Page on #{env}" do
+
       @eyes.test(
 
         app_name: 'Echo', 
@@ -84,6 +93,35 @@ describe "Echo Test Suite" do
 
         driver.get(@base_url + "/select-school.html")
         @eyes.check_window('Select School')
+
+        driver.quit
+      end
+
+    end
+
+    it "tests the Survey Editor Page on #{env}" do
+      @eyes.test(
+
+        app_name: 'Echo', 
+        test_name: 'Survey Editor Test', 
+        viewport_size: { width: 1024, height: 768 },
+        driver: driver_for(env)
+
+      ) do |driver|
+
+        driver.get(@base_url + "/survey-edit.html")
+        @eyes.check_window('Survey Edit')
+
+        driver.execute_script(%|
+          document.querySelector('.survey-edit__content').scrollTop = 600
+        |)
+        driver.find_element(:css, "label[for=radio1]").click
+        @eyes.check_window('Survey Question Section 1')
+
+        driver.execute_script(%|
+          document.querySelector('.survey-edit__content').scrollTop = 1600
+        |)
+        @eyes.check_window('Survey Question Section 2')
 
         driver.quit
       end
