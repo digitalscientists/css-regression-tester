ce9dc35f23eccdb5f63dcd8c561ff8e3cc573fd7
Fadi Eliya <fadi.eliya@digitalscientists.com>
2015-03-05 17:52:54 -0500

    [#88709808] Add prompt to test locally or remotely

--- echo.rb
+++ echo.rb
@@ -9,21 +9,45 @@ access_key = ENV['SAUCE_KEY']
 SAUCE_URL  = "http://#{username}:#{access_key}@ondemand.saucelabs.com/wd/hub"
 
 file = File.read('./environments.json')
-environments = JSON.parse(file)
+Environments = JSON.parse(file)
+
+def get_user_input
+  puts "Run locally or on Sauce Labs?"
+  puts "f: firefox"
+  puts "c: chrome"
+  puts "s: Sauce Labs"
+  $stdin.gets.chomp.downcase
+end
 
 def driver_for(env)
-  caps = Selenium::WebDriver::Remote::Capabilities.chrome
-  caps[:screenResolution] = "1280x1024"
-  caps.platform, caps.browser_name, caps.version = env
 
-  Selenium::WebDriver.for(:remote,
-    url: SAUCE_URL, 
-    desired_capabilities: caps
-  )
-  # Selenium::WebDriver.for(:firefox)
+  if Key == 's'
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome
+    caps[:screenResolution] = "1280x1024"
+    caps.platform, caps.browser_name, caps.version = env
+
+    Selenium::WebDriver.for(:remote,
+      url: SAUCE_URL, 
+      desired_capabilities: caps
+    )
+  else
+    Selenium::WebDriver.for(Key == 'c' ? :chrome : :firefox )
+  end
+end
+
+def run_tests
+  if Key == 's'
+    Environments.each do |env|
+      yield(env)
+    end
+  else
+    yield ['Locally', Key, 'latest']
+  end
 end
 
 describe "Echo Test Suite" do
+
+  Key = get_user_input
 
   before(:all) do
 
@@ -36,7 +60,6 @@ describe "Echo Test Suite" do
 
   before(:each) do
     @verification_errors = []
-
   end
   
   after(:each) do
@@ -46,7 +69,7 @@ describe "Echo Test Suite" do
   after(:all) do
   end
   
-  environments.each do |env|
+  run_tests do |env|
 
     it "tests the dashboard on #{env}" do
 
